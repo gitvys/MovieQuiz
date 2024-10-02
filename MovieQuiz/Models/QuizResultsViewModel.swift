20ec89a46d547c04e3b385221afbbae73e1ad195
vysoko <sokolikvlad@yandex.ru>
2024-10-02 21:50:02 +0300

    промежуточный вариант после добавления делегата и инъекции

---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Владислав Соколов on 07.09.2024.
+//
+
+import UIKit
+
+// для состояния "Результат квиза"
+struct QuizResultsViewModel {
+  // строка с заголовком алерта
+  let title: String
+  // строка с текстом о количестве набранных очков
+  let text: String
+  // текст для кнопки алерта
+  let buttonText: String
+}
---
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionFactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by Владислав Соколов on 17.09.2024.
+//
+
+import Foundation
+
+protocol QuestionFactoryProtocol {
+    func requestNextQuestion()
+}
